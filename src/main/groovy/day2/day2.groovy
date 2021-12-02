bd62acff95da577ab7d13e33b82f36322c2fc5ad
Viktor Kovacs <crusader8@gmail.com>
2021-12-02 08:36:28 +0100

    day2 part2

--- src/main/groovy/day2/day2.groovy
+++ src/main/groovy/day2/day2.groovy
@@ -1,7 +1,7 @@
 package day2
 
 def inputFile = new File("../../resources/day2/input")
-def lines = inputFile.readLines()
+lines = inputFile.readLines()
 
 //part 1
 
@@ -12,21 +12,59 @@ static def movement(String movementString) {
 }
 
 static def positionChange(Tuple movement) {
-    switch (movement.get(0)) {
-        case "forward": return new Tuple(movement.get(1), 0)
-        case "down": return new Tuple(0, movement.get(1))
-        case "up": return new Tuple(0, -movement.get(1))
+    def command = movement.get(0)
+    def value = movement.get(1)
+    
+    switch (command) {
+        case "forward": return new Tuple(value, 0)
+        case "down": return new Tuple(0, value)
+        case "up": return new Tuple(0, -value)
     }
 }
 
-def horizontalPosition = 0
-def depth = 0
+def part1() {
+    def horizontalPosition = 0
+    def depth = 0
 
-for (def line : lines) {
-    def movement = movement(line)
-    def change = positionChange(movement)
-    horizontalPosition += change.get(0)
-    depth += change.get(1)
+    for (def line : lines) {
+        def movement = movement(line)
+        def change = positionChange(movement)
+        horizontalPosition += change.get(0)
+        depth += change.get(1)
+    }
+
+    println horizontalPosition * depth
 }
 
-println horizontalPosition * depth
+part1()
+
+//part 2
+
+static def positionChangeWithAim(Tuple movement, def aim) {
+    def command = movement.get(0)
+    def value = movement.get(1)
+
+    switch (command) {
+        case "forward": return new Tuple(value, aim * value, 0)
+        case "down": return new Tuple(0, 0, value)
+        case "up": return new Tuple(0, 0, -value)
+    }
+}
+
+def part2() {
+    def horizontalPosition = 0
+    def depth = 0
+    def aim = 0
+
+    for (def line : lines) {
+        def movement = movement(line)
+        def change = positionChangeWithAim(movement, aim)
+        horizontalPosition += change.get(0)
+        depth += change.get(1)
+        aim += change.get(2)
+    }
+
+    println horizontalPosition * depth
+}
+
+part2()
