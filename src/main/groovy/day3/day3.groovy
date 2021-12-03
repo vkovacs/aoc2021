53b596920f6735d5d696dd55dacd174c3248a6da
Viktor Kovacs <crusader8@gmail.com>
2021-12-03 10:37:49 +0100

    refactor part1

--- src/main/groovy/day3/day3.groovy
+++ src/main/groovy/day3/day3.groovy
@@ -1,6 +1,6 @@
 package day3
 
-def inputFile = new File("../../resources/day3/input")
+def inputFile = new File("../../resources/day3/input-test")
 
 List<List<Integer>> matrix = []
 inputFile.splitEachLine("", {
@@ -8,33 +8,39 @@ inputFile.splitEachLine("", {
 })
 
 def column(List<List<Integer>> matrix, def number) {
-    def column = []
-
     matrix.collect({
         it.get(number)
     })
 }
 
-var gammaRateString = ""
-var epsilonRateString = ""
+def part1(def matrix) {
+    var gammaRateString = ""
+    var epsilonRateString = ""
 
-(0..<matrix[0].size()).each {
-    def column = column(matrix, it)
+    (0..<matrix[0].size()).each {
+        def column = column(matrix, it)
 
-    def zeros = column.count(0)
-    def ones = matrix.size() - zeros
+        def zeros = column.count(0)
+        def ones = matrix.size() - zeros
 
-    if (zeros > ones) {
-        gammaRateString += 1
-        epsilonRateString += 0
-    } else {
-        gammaRateString += 0
-        epsilonRateString += 1
+        if (zeros > ones) {
+            gammaRateString += 1
+            epsilonRateString += 0
+        } else {
+            gammaRateString += 0
+            epsilonRateString += 1
+        }
     }
+
+    def gammaRate = Integer.parseInt(gammaRateString, 2)
+    def epsilonRate = Integer.parseInt(epsilonRateString, 2)
+
+    gammaRate * epsilonRate
 }
 
-def gammaRate = Integer.parseInt(gammaRateString, 2)
-def epsilonRate = Integer.parseInt(epsilonRateString, 2)
+println(part1(matrix))
 
-println gammaRate * epsilonRate
+
+// part2
+
 
