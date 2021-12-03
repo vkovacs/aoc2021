dcdee188f654c09774f39744ec075ce9484c705a
Viktor Kovacs <crusader8@gmail.com>
2021-12-03 11:06:56 +0100

    day3 part2

--- src/main/groovy/day3/day3.groovy
+++ src/main/groovy/day3/day3.groovy
@@ -1,6 +1,6 @@
 package day3
 
-def inputFile = new File("../../resources/day3/input-test")
+def inputFile = new File("../../resources/day3/input")
 
 List<List<Integer>> matrix = []
 inputFile.splitEachLine("", {
@@ -43,4 +43,49 @@ println(part1(matrix))
 
 // part2
 
+def eliminate(List<List<Integer>> matrix, int columnNumber, String selector) {
+    if (matrix.size() == 1) {
+        return matrix.get(0).join()
+    } else {
+        def column = column(matrix, columnNumber)
+
+        def zeros = column.count(0)
+        def ones = matrix.size() - zeros
+
+        if (selector == "most") {
+            if (ones >= zeros) {
+                eliminate(matrix.findAll({
+                    it.get(columnNumber) == 1
+                }), columnNumber + 1, "most")
+            } else {
+                eliminate(matrix.findAll({
+                    it.get(columnNumber) == 0
+                }), columnNumber + 1, "most")
+            }
+        } else {
+            if (ones >= zeros) {
+                eliminate(matrix.findAll({
+                    it.get(columnNumber) == 0
+                }), columnNumber + 1, "least")
+            } else {
+                eliminate(matrix.findAll({
+                    it.get(columnNumber) == 1
+                }), columnNumber + 1, "least")
+            }
+        }
+
+    }
+}
+
+def part2(def matrix) {
+    String oxygenGeneratorRatingString = eliminate(matrix, 0, "most")
+    String co2ScrubberRatingString = eliminate(matrix, 0, "least")
+
+    def oxygenGeneratorRating = Integer.parseInt(oxygenGeneratorRatingString, 2)
+    def co2ScrubberRating = Integer.parseInt(co2ScrubberRatingString, 2)
+
+    oxygenGeneratorRating * co2ScrubberRating
+}
+
+println(part2(matrix))
 
