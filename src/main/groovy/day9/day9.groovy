b15b379495ce30aff2eccdf0f8c9732dfb1da0c1
Viktor Kovacs <crusader8@gmail.com>
2021-12-09 10:03:47 +0100

    day9 part2

--- src/main/groovy/day9/day9.groovy
+++ src/main/groovy/day9/day9.groovy
@@ -1,5 +1,8 @@
 package day9
 
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
 def inputFile = new File("../../resources/day9/input")
 
 List<List<Integer>> heightMap = []
@@ -7,31 +10,77 @@ inputFile.eachLine {
     line -> heightMap.add(line.split("").collect { it as int })
 }
 
-static List<Integer> adjacentLocationHeights(List<List<Integer>> heightMap, int row, int col) {
-    int rowSize = heightMap.size()
-    int colSize = heightMap[0].size()
-    def adjacentLocationHeights = []
-
-    if (row < rowSize - 1) adjacentLocationHeights.add(heightMap[row + 1][col])
-    if (row > 0) adjacentLocationHeights.add(heightMap[row - 1][col])
-    if (col < colSize - 1) adjacentLocationHeights.add(heightMap[row][col + 1])
-    if (col > 0) adjacentLocationHeights.add(heightMap[row][col - 1])
-
-    return adjacentLocationHeights
+@ToString
+@EqualsAndHashCode
+class Position {
+    int row, col
 }
 
 static def part1(List<List<Integer>> heightMap) {
     int riskLevel = 0
+    Set<Position> lowPointPositions = []
 
     for (i in 0..<heightMap.size()) {
         for (j in 0..<heightMap[0].size()) {
             int point = heightMap[i][j]
             if (point < adjacentLocationHeights(heightMap, i, j).min()) {
+                lowPointPositions.add(new Position(row: i, col: j))
                 riskLevel += 1 + point
             }
         }
     }
-    riskLevel
+    new Tuple(riskLevel, lowPointPositions)
 }
 
-println part1(heightMap)
+static List<Integer> adjacentLocationHeights(List<List<Integer>> heightMap, int row, int col) {
+    adjacentLocations(heightMap, row, col).collect{heightMap[it.row][it.col]}
+}
+
+static Set<Position> adjacentLocations(List<List<Integer>> heightMap, int row, int col) {
+    int rowSize = heightMap.size()
+    int colSize = heightMap[0].size()
+    def adjacentLocationHeights = []
+
+    if (row < rowSize - 1) adjacentLocationHeights.add(new Position(row: row + 1, col: col))
+    if (row > 0) adjacentLocationHeights.add(new Position(row: row - 1, col: col))
+    if (col < colSize - 1) adjacentLocationHeights.add(new Position(row: row, col: col + 1))
+    if (col > 0) adjacentLocationHeights.add(new Position(row: row, col: col - 1))
+
+    return adjacentLocationHeights
+}
+
+def (int riskLevel, Set<Position> lowPointPositions) = part1(heightMap)
+
+println riskLevel
+
+static def part2(List<List<Integer>> heightMap, Set<Position> lowPointPositions) {
+    List<Integer> basinSizes = []
+    lowPointPositions.each {
+        Set<Position> basinPositions = findBasinPositions(heightMap, new HashSet<Position>(), adjacentLocations(heightMap, it.row, it.col).findAll {heightMap[it.row][it.col] != 9})
+        basinSizes.add(basinPositions.size())
+    }
+
+    basinSizes.sort()
+    return basinSizes[basinSizes.size() - 1] * basinSizes[basinSizes.size() - 2] * basinSizes[basinSizes.size() - 3]
+}
+
+static Set<Position> findBasinPositions(List<List<Integer>> heightMap, Set<Position> visitedPositions, Set<Position> newPositions) {
+    visitedPositions.addAll(newPositions)
+
+    Set<Position> newNewPoints = []
+    newPositions.each {
+        adjacentLocations(heightMap, it.row, it.col).findAll { heightMap[it.row][it.col] != 9 }.each {
+            if (!visitedPositions.contains(it)) {
+                newNewPoints.add(it)
+            }
+        }
+    }
+
+    if (newNewPoints.isEmpty()) {
+        return visitedPositions
+    } else {
+        findBasinPositions(heightMap, visitedPositions, newNewPoints)
+    }
+}
+
+println part2(heightMap, lowPointPositions)
