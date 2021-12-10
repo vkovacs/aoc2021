2fb6f37957258c8880bb3061cf7e668a57857638
Viktor Kovacs <crusader8@gmail.com>
2021-12-10 09:19:26 +0100

    day10 part2

--- src/main/groovy/day10/day10.groovy
+++ src/main/groovy/day10/day10.groovy
@@ -2,24 +2,26 @@ package day10
 
 def inputFile = new File("../../resources/day10/input")
 
-scoreMap = [")": 3, "]": 57, "}": 1197, ">": 25137]
 openers = ["(", "[", "{", "<"]
 closers = [")", "]", "}", ">"]
 
 def part1(inputFile) {
+    def scoreMap = [")": 3, "]": 57, "}": 1197, ">": 25137]
+
     int score = 0
     inputFile.eachLine {
         findSyntaxError(it).ifPresent(e -> score += scoreMap[e])
     }
     score
 }
+
 println part1(inputFile)
 
 Optional<String> findSyntaxError(String line) {
     def tokens = line.split("")
-    Stack<String> stack = new Stack()
+    def stack = []
 
-    for (String token : tokens)
+    for (String token : tokens) {
         if (openers.contains(token)) {
             stack.push(token)
         } else if (closers.contains(token)) {
@@ -27,10 +29,8 @@ Optional<String> findSyntaxError(String line) {
             if (!isPair(previousOpener, token)) {
                 return Optional.of(token)
             }
-        } else {
-            throw new IllegalArgumentException()
         }
-
+    }
 
     Optional.empty()
 }
@@ -40,4 +40,56 @@ boolean isPair(String opener, String closer) {
             opener == "[" && closer == "]" ||
             opener == "{" && closer == "}" ||
             opener == "<" && closer == ">"
+}
+
+def part2(inputFile) {
+    List<String> incompleteLines = inputFile.findAll {
+        findSyntaxError(it).isEmpty()
+    }
+
+    def scores = incompleteLines.collect {
+        score(findMissingClosers(it))
+    }.sort()
+
+    return scores.get(scores.size() / 2 as int)
+}
+
+println part2(inputFile)
+
+long score(List<String> missingClosers) {
+    def scoreMap = [")": 1, "]": 2, "}": 3, ">": 4]
+
+    long score = 0
+    missingClosers.each {
+        score = score * 5 + scoreMap[it]
+    }
+
+    score
+}
+
+String myPair(String opener) {
+    switch (opener) {
+        case "(": return ")"
+        case "[": return "]"
+        case "{": return "}"
+        case "<": return ">"
+        default: throw new IllegalArgumentException()
+    }
+}
+
+List<String> findMissingClosers(String line) {
+    def tokens = line.split("")
+    def stack = []
+
+    for (String token : tokens) {
+        if (openers.contains(token)) {
+            stack.push(token)
+        } else if (closers.contains(token)) {
+            stack.pop()
+        }
+    }
+
+    stack.collect {
+        myPair(it)
+    }
 }
