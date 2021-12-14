6ea611ef847546f4b33ad550176cd558751c8d50
Viktor Kovacs <crusader8@gmail.com>
2021-12-14 10:35:55 +0100

    day 14 part 1 iterative

--- src/main/groovy/day14/day14.groovy
+++ src/main/groovy/day14/day14.groovy
@@ -2,31 +2,30 @@ package day14
 
 import groovy.transform.Field
 
-@Field def inputFile = new File("../../resources/day14/input")
-@Field def lines = inputFile.readLines()
-@Field Map<String, String> insertionPairs = lines.subList(2, lines.size()).collectEntries{
+@Field def lines = new File("../../resources/day14/input").readLines()
+@Field Map<String, String> insertionPairs = lines.subList(2, lines.size()).collectEntries {
     def split = it.split(" -> ")
     [(split[0]): split[1]]
 }
 
-StringBuilder expand(String template, StringBuilder accumulator) {
-    if (template.length() < 2) return accumulator.append(template[0])
-
-    def (letter0, letter1) = [template[0], template[1]]
-    def product = insertionPairs.get(letter0 + letter1)
-    if (product != null) accumulator.append(letter0).append(product)
-
-    expand(template.substring(1), accumulator)
+String expand(String template) {
+    StringBuilder accumulator = new StringBuilder()
+    for (int i in 0..<template.size() - 1) {
+        def (letter0, letter1) = [template[i], template[i + 1]]
+        def product = insertionPairs.get(letter0 + letter1)
+        if (product != null) accumulator.append(letter0).append(product)
+    }
+    accumulator.append(template[template.size() - 1]) toString()
 }
 
 println part1(lines[0])
 
 def part1(String template) {
     10.times {
-        template = expand(template, new StringBuilder()).toString()
+        template = expand(template).toString()
     }
 
     def letterCount = template.split("").countBy { it }
-    return letterCount.max {it.value}.value - letterCount.min {it.value}.value
+    letterCount.max { it.value }.value - letterCount.min { it.value }.value
 }
 
