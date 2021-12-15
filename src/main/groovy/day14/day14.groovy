8d2d2f782fe4f9b016aaba85bf87502ca2a556ce
Viktor Kovacs <crusader8@gmail.com>
2021-12-15 13:40:21 +0100

    day14 part2

--- src/main/groovy/day14/day14.groovy
+++ src/main/groovy/day14/day14.groovy
@@ -29,3 +29,41 @@ def part1(String template) {
     letterCount.max { it.value }.value - letterCount.min { it.value }.value
 }
 
+def part2(String template) {
+    Map<String, Long> pairCountMap = [:]
+    for (int i in 0..<template.size() - 1) {
+        def (letter0, letter1) = [template[i], template[i + 1]]
+        pairCountMap[letter0 + letter1] = pairCountMap.get(letter0 + letter1, 0L) + 1
+    }
+
+    40.times {
+        pairCountMap = expand2(pairCountMap)
+    }
+
+    Map<String, Long> letterCountMap = [:]
+
+    pairCountMap.each {
+        def (String letter0, String letter1) = it.key.split("")
+        letterCountMap[letter0] = letterCountMap.get(letter0, 0L) + it.value
+        letterCountMap[letter1] = letterCountMap.get(letter1, 0L) + it.value
+    }
+
+    letterCountMap.each {
+        letterCountMap[it.key] = Math.ceil(letterCountMap[it.key] / 2) as long
+    }
+
+    letterCountMap.max { it.value }.value as Long - letterCountMap.min { it.value }.value as Long
+}
+
+println part2(lines[0])
+
+Map<String, Long> expand2(LinkedHashMap<String, Long> countMap) {
+    Map<String, Long> newCountMap = [:]
+    for (entry in countMap) {
+        def product = insertionPairs.get(entry.key)
+        def (String letter0, String letter1) = entry.key.split("")
+        newCountMap[letter0 + product] = newCountMap.get(letter0 + product, 0L) + entry.value
+        newCountMap[product + letter1] = newCountMap.get(product + letter1, 0L) + entry.value
+    }
+    newCountMap
+}
